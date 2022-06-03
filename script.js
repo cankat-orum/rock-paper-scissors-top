d8794a90991acfaf1d2aa181e151d7171d997d7c
Cankat <jankatorumanp@gmail.com>
2022-06-03 17:55:56 +0300

    EDIT game logic (SUCCESS)

--- script.js
+++ script.js
@@ -6,41 +6,69 @@ let returnItem = () => {
   return items[Math.floor(Math.random() * items.length)];
 };
 
-function gameLoop() {
-  let playerChoice = prompt("ROCK PAPER SCISSORS");
-  if (counterMe || counterPc === 5) {
-    if (counterMe === 5) {
-      alert("YOU WON");
-    } else if (counterPc === 5) {
-      alert("PC WON");
-    }
-  } else if (playerChoice == returnItem) {
-    gameLoop();
-  } else if (playerChoice == "rock") {
-    if (returnItem == "scissors") {
-      counterMe++;
-      gameLoop;
-    } else if (returnItem == "paper") {
-      counterPc++;
-      gameLoop;
-    }
-  } else if (playerChoice == "paper") {
-    if (returnItem == "rock") {
-      counterMe++;
-      gameLoop;
-    } else if (returnItem == "scissors") {
-      counterPc++;
-      gameLoop;
-    }
-  } else if (playerChoice == "scissors") {
-    if (returnItem == "paper") {
-      counterMe++;
-      gameLoop;
-    } else if (returnItem == "rock") {
-      counterPc++;
-      gameLoop;
+let showScore = () => {
+  console.log("ME" + counterMe + "-" + counterPc + "PC");
+};
+
+let checkWin = () => {
+  if (counterMe == 5) {
+    alert("YOU WON!!!");
+  }
+  if (counterPc == 5) {
+    alert("YOU LOST PC WON");
+  }
+};
+
+let gameLoop = async () => {
+  while (counterMe < 5 && counterPc < 5) {
+    let userItem = await prompt("ROCK PAPER SCISSORS");
+    let pcItem = returnItem();
+    if (userItem == "rock" || userItem == "paper" || userItem == "scissors") {
+      if (userItem == pcItem) {
+        console.log("ME: " + userItem + "---" + pcItem + " :PC");
+        console.log("ME: " + counterMe + "---" + counterPc + " :PC");
+        console.log("TIE");
+      } else if (userItem == "rock") {
+        if (pcItem == "paper") {
+          counterPc++;
+          console.log("ME: " + userItem + "---" + pcItem + " :PC");
+          console.log("ME: " + counterMe + "---" + counterPc + " :PC");
+          checkWin();
+        } else if (pcItem == "scissors") {
+          counterMe++;
+          console.log("ME: " + userItem + "---" + pcItem + " :PC");
+          console.log("ME: " + counterMe + "---" + counterPc + " :PC");
+          checkWin();
+        }
+      } else if (userItem == "paper") {
+        if (pcItem == "scissors") {
+          counterPc++;
+          console.log("ME: " + userItem + "---" + pcItem + " :PC");
+          console.log("ME: " + counterMe + "---" + counterPc + " :PC");
+          checkWin();
+        } else if (pcItem == "rock") {
+          counterMe++;
+          console.log("ME: " + userItem + "---" + pcItem + " :PC");
+          console.log("ME: " + counterMe + "---" + counterPc + " :PC");
+          checkWin();
+        }
+      } else if (userItem == "scissors") {
+        if (pcItem == "rock") {
+          counterPc++;
+          console.log("ME: " + userItem + "---" + pcItem + " :PC");
+          console.log("ME: " + counterMe + "---" + counterPc + " :PC");
+          checkWin();
+        } else if (pcItem == "paper") {
+          counterMe++;
+          console.log("ME: " + userItem + "---" + pcItem + " :PC");
+          console.log("ME: " + counterMe + "---" + counterPc + " :PC");
+          checkWin();
+        }
+      }
+    } else {
+      alert("TYPE PROPERLY");
     }
   }
-}
+};
 
 gameLoop();
